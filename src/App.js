fa5809cea7067017875a953d59bcc113c385d192
dsilva-d <dsilva.d@husky.neu.edu>
2023-02-12 21:10:11 -0500

    adding image and esc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import whiskyimg from './whiskybottle.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>D'Stilled</h1>
+        <h5>Making Whisky-Making Simpler Than Making Whisky</h5>
+        <h3>(made the right way)</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the D'Stillary Homepage! Grab a glass, take a seat, and ease back into that chair, and have a seat. You can relax, sit down. Drink up and take a pick from our 4 delicious and scrumptioius options: ... Actually, options to come. We're gonna have options soon. This is one of the bottles we put the whisky in:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={whiskyimg} alt="whiskyimg"/>
       </header>
     </div>
   );
